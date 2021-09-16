dd2c55befc7dedf17d5bda1bf2c54bc33dd9cf64
Hugo Leonardo da Silva Santos <hugo.desenvolvedor@gmail.com>
2021-09-16 16:03:11 -0300

    feat: create the pagination component

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -2,6 +2,7 @@ import { Flex, Text, Icon, Box, Heading, Button, Table, Checkbox, Thead, Th, Tr,
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 
 import { Header } from '../../components/Header'
+import { Pagination } from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar'
 
 
@@ -64,6 +65,7 @@ export default function UserList() {
                             </Tr>
                         </Tbody>
                     </Table>
+                    <Pagination />
                 </Box> 
             </Flex>
         </Box>
