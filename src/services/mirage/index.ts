c262a603ac45a679789e3ba766ba57537a8b5552
Hugo Leonardo da Silva Santos <hugo.desenvolvedor@gmail.com>
2021-09-21 17:56:38 -0300

    feat: create pagination miragejs script

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,4 +1,4 @@
-import { createServer, Factory, Model } from 'miragejs';
+import { createServer, Factory, Model, Response } from 'miragejs';
 import faker from 'faker';
 
 type User = {
@@ -29,7 +29,7 @@ export function makeServer() {
         },
 
         seeds(server) {
-            server.createList('user', 10)
+            server.createList('user', 200)
         },
 
         routes() {
@@ -37,8 +37,28 @@ export function makeServer() {
             
             this.timing = 750;
 
-            // MirageJS Shorthands to CREATE and RETRIEVE
-            this.get('/users');
+            // MirageJS Shorthand to retrieve and paginate users
+            this.get('/users', function (schema, request) {
+                const { page = 1, per_page = 10 } = request.queryParams
+
+                const total = schema.all('user').length
+
+                const pageStart = (Number(page) - 1) * Number(per_page)
+                const pageEnd = pageStart + Number(per_page)
+
+                const users = this.serialize(schema.all('user'))
+                    .users.slice(pageStart, pageEnd)
+
+                return new Response(
+                    200,
+                    {
+                        'x-total-count': String(total)
+                    },
+                    { users }
+                )
+            });
+
+            // MirageJS Shorthand to create a user
             this.post('/users');
 
             // Clean the namespace to use the Next default api route (pages/api)
